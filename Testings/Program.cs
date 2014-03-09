92f5bc3027f807897ccdf6d3ff4de7e390a0fedf
Matan <smatan84@gmail.com>
2014-03-09 22:37:35 +0200

    Project Added

---
+++ Testings/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DatabaseIndex.Entities;
+using DatabaseIndex.Logic;
+using System;
+
+namespace Testings
+{
+    class Program
+    {
+        static XmlDataAccess<OrderItem> _dataAccess;
+        private static IndexService<OrderItem> _service;
+
+        static void Main(string[] args)
+        {
+            _dataAccess = new XmlDataAccess<OrderItem>(Directory.GetCurrentDirectory() + "\\MyTableData.xml");
+            _service = new IndexService<OrderItem>(_dataAccess.GetData());
+
+            _service.CreateIndex("Quantity", item => item.Quantity);
+
+
+            List<OrderItem> search = _service.RetrieveData("Quantity", 12, 20).ToList();
+
+            _service.CreateIndex("ProductID", item => item.ProductID);
+
+            //search = _service.RetrieveData("UnitPrice", 5m, 10m).ToList();
+
+            _service.CreateIndex("OrderID", item => item.OrderID);
+
+            var row = _service.RetrieveData("OrderID", 10000, 10500);
+
+           // Console.WriteLine(_service.DropIndex("OrderID"));
+
+            var row2 = _dataAccess.GetData().First();
+
+            _dataAccess.Delete(row2);
+
+            OrderItem newrow = new OrderItem
+            {
+                Discount = 10,
+                OrderID = 10,
+                ProductID = 10,
+                Quantity = 0,
+                UnitPrice = 100
+            };
+
+            _dataAccess.Insert(newrow);
+
+            var newRowSearch = _service.RetrieveData("Quantity", 0);
+
+            var orQuery = _service.RetrieveData("ProductID", "OrderID", 10, SearchOperation.Or);
+            Console.Read();
+        }
+    }
+}
---
+++ Testings/XmlDataAccess.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using DatabaseIndex.Entities;
+
+namespace Testings
+{
+    public class XmlDataAccess<T>
+    {
+        private ICollection<T> _data;
+        private string _path;
+
+        public XmlDataAccess(string path)
+        {
+            _path = path;
+            InitData();
+        }
+
+        private void InitData()
+        {
+            _data = new List<T>();
+            XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+            FileStream reader = new FileStream(_path, FileMode.Open);
+            _data = (List<T>)serializer.Deserialize(reader);
+        }
+
+        public IEnumerable<T> GetData()
+        {
+            return _data;
+        }
+
+        public void Insert(T data)
+        {
+            try
+            {
+                _data.Add(data);
+                IndexEvent.IndexEventHandler(this, new IndexEventArgs //firing up the event to update the index
+                {
+                    State = DataState.Add,
+                    Row = data
+                });
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void Delete(T data)
+        {
+            try
+            {
+                _data.Remove(data);
+                IndexEvent.IndexEventHandler(this, new IndexEventArgs //firing up the event to update the index
+                {
+                    State = DataState.Remove,
+                    Row = data
+                });
+            }
+            catch (Exception e)
+            {
+                
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
